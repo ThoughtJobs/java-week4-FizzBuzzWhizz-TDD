cbc44321e0b3eab96bb61e945c7c5276ffc4ac8e
htao <htao@thoughtworks.com>
2018-04-10 11:30:36 +0800

    [taohui] refactor logic about handling number is multiple of special number

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -23,21 +23,20 @@ public class FizzBuzzGame {
     public String translate(int number) {
         if(new Integer(number).toString().contains(new Integer(FizzNumber).toString()))
             return "Fizz";
-        if(number % FizzNumber == 0 && number % BuzzNumber == 0 && number % WhizzNumber == 0)
-            return "FizzBuzzWhizz";
-        if(number % FizzNumber == 0 && number % BuzzNumber == 0)
-            return "FizzBuzz";
-        if(number % FizzNumber == 0 && number % WhizzNumber == 0)
-            return "FizzWhizz";
-        if(number % BuzzNumber == 0 && number % WhizzNumber == 0)
-            return "BuzzWhizz";
-        if(number % FizzNumber == 0)
-            return "Fizz";
-        if(number % BuzzNumber == 0)
-            return "Buzz";
-        if(number % WhizzNumber == 0)
-            return "Whizz";
+        if(number % FizzNumber == 0 || number % BuzzNumber == 0 || number % WhizzNumber == 0)
+            return HandleTranslationForMultipleCases(number);
         return new Integer(number).toString();
+    }
+
+    private String HandleTranslationForMultipleCases(int number) {
+        String output = "";
+        if(number % FizzNumber == 0)
+            output += "Fizz";
+        if(number % BuzzNumber == 0)
+            output += "Buzz";
+        if(number % WhizzNumber == 0)
+            output += "Whizz";
+        return output;
     }
 }
 
