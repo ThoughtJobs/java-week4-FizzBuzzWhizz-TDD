1c889bb2d8463d2b7faeb03eabb96d655353555b
htao <htao@thoughtworks.com>
2018-04-16 10:18:29 +0800

    [taohui] modify readme

--- src/main/java/com/thoughtworks/MainApplication.java
+++ src/main/java/com/thoughtworks/MainApplication.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 
 public class MainApplication {
     public static void main(String[] args){
-        System.out.println("Let's play FizzBuzzWhizz Game!\nPlease input three special numbers which are smaller than 10, divided by comma.");
+        System.out.println("Let's play FizzBuzzWhizz Game!");
+        System.out.println("Please input three special numbers which are smaller than 10, divided by comma.");
         Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
         String[] inputNumbers = number.split(",");
         FizzBuzzGame game = new FizzBuzzGame(Integer.parseInt(inputNumbers[0]),
                 Integer.parseInt(inputNumbers[1]),
                 Integer.parseInt(inputNumbers[2]));
-        System.out.println("Please input the total student number\n");
+        System.out.println("Please input the total student number:");
         game.start(scanner.nextInt());
         List<String> results = game.getResults();
         System.out.println(results.toString());
